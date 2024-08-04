38d29d465fb33d4392d8cc94bf180dcfb17c167e
Kaylee Reed <kaylee@kaylee-macbookAir.local>
2024-08-04 15:35:43 +0200

    NvimTree fix

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -113,17 +113,32 @@ return require('packer').startup(function(use)
   use 'NeogitOrg/neogit'
   use("petertriho/nvim-scrollbar")
   use {
-    'nvim-tree/nvim-tree.lua',
-    requires = {
-      'nvim-tree/nvim-web-devicons', -- optional, for file icons
-    },
-    tag = 'nightly' -- optional, updated every week. (see issue #1193)
+    "kyazdani42/nvim-tree.lua",
+    requires = "kyazdani42/nvim-web-devicons" ,
+    wants = "nvim-web-devicons",
+    config = function()
+      require("nvim-web-devicons").setup()
+
+      require("nvim-tree").setup {
+        hijack_cursor = true,
+        view = {
+          width = 25
+        }
+      }
+    end
   }
-  -- use 'folke/tokyonight.nvim'
+  use 'folke/tokyonight.nvim'
   use { "catppuccin/nvim", as = "catppuccin" }
   require("neogit").setup();
   require("ccc").setup();
-  require("nvim-tree").setup()
+  
+  -- disable netrw at the very start of your init.lua
+  vim.g.loaded_netrw = 1
+  vim.g.loaded_netrwPlugin = 1
+
+  -- optionally enable 24-bit colour
+  vim.opt.termguicolors = true
+
   require("trouble").setup()
   require("Comment").setup()
   require("colorizer").setup()
@@ -162,41 +177,41 @@ return require('packer').startup(function(use)
     },
     mappings = {}
 }
-  -- require("tokyonight").setup({
-  --   -- your configuration comes here
-  --   -- or leave it empty to use the default settings
-  --   style = "day", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-  --   light_style = "day", -- The theme is used when the background is set to light
-  --   transparent = true, -- Enable this to disable setting the background color
-  --   terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
-  --   styles = {
-  --     -- Style to be applied to different syntax groups
-  --     -- Value is any valid attr-list value for `:help nvim_set_hl`
-  --     comments = { italic = true },
-  --     keywords = { italic = true },
-  --     functions = {},
-  --     variables = {},
-  --     -- Background styles. Can be "dark", "transparent" or "normal"
-  --     sidebars = "dark", -- style for sidebars, see below
-  --     floats = "dark", -- style for floating windows
-  --   },
-  --   sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
-  --   day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
-  --   hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
-  --   dim_inactive = false, -- dims inactive windows
-  --   lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
-  --
-  --   --- You can override specific color groups to use other groups or a hex color
-  --   --- function will be called with a ColorScheme table
-  --   ---@param colors ColorScheme
-  --   on_colors = function(colors) end,
-  --
-  --   --- You can override specific highlights to use other groups or a hex color
-  --   --- function will be called with a Highlights and ColorScheme table
-  --   ---@param highlights Highlights
-  --   ---@param colors ColorScheme
-  --   on_highlights = function(highlights, colors) end,
-  -- })
+  require("tokyonight").setup({
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+    light_style = "storm", -- The theme is used when the background is set to light
+    transparent = true, -- Enable this to disable setting the background color
+    terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+    styles = {
+      -- Style to be applied to different syntax groups
+      -- Value is any valid attr-list value for `:help nvim_set_hl`
+      comments = { italic = true },
+      keywords = { italic = true },
+      functions = {},
+      variables = {},
+      -- Background styles. Can be "dark", "transparent" or "normal"
+      sidebars = "dark", -- style for sidebars, see below
+      floats = "dark", -- style for floating windows
+    },
+    sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+    day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+    hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
+    dim_inactive = false, -- dims inactive windows
+    lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
+
+    --- You can override specific color groups to use other groups or a hex color
+    --- function will be called with a ColorScheme table
+    ---@param colors ColorScheme
+    on_colors = function(colors) end,
+
+    --- You can override specific highlights to use other groups or a hex color
+    --- function will be called with a Highlights and ColorScheme table
+    ---@param highlights Highlights
+    ---@param colors ColorScheme
+    on_highlights = function(highlights, colors) end,
+  })
   require("dressing").setup({
     input = {
       -- Set to false to disable the vim.ui.input implementation
