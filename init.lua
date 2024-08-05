1b50febe61e60ea29def5d7257675e100b651a44
Kaylee Reed <kaylee@kaylee-macbookAir.local>
2024-08-05 12:23:16 +0200

    Adding default plugins lua files

--- init.lua
+++ init.lua
@@ -1,2 +1,16 @@
 require("packer-setup")
 require("core.options")
+
+require("plugins.autopairs")
+require("plugins.gitsigns")
+require("plugins.lualine")
+require("plugins.nvim-tree")
+require("plugins.treesitter")
+require("plugins.comment")
+require("plugins.nvim-cmp")
+require("plugins.telescope")
+require("plugins.autopairs")
+
+require("plugins.lsp.mason")
+require("plugins.lsp.lspsaga")
+require("plugins.lsp.lspconfig")
