dc3ac98d2980cc4c361f69e8b3640b5c8489b8e3
Kaylee Reed <kaylee@kaylee-macbookAir.local>
2024-08-05 15:49:02 +0200

    Adding neoscroll

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 require("plugin-setup")
 require("core.options")
+require("core.keymaps")
 
 require("plugins.autopairs")
 require("plugins.gitsigns")
@@ -10,7 +11,9 @@ require("plugins.comment")
 require("plugins.nvim-cmp")
 require("plugins.telescope")
 require("plugins.autopairs")
+require("plugins.neoscroll")
 
 require("plugins.lsp.mason")
 require("plugins.lsp.lspsaga")
 require("plugins.lsp.lspconfig")
+
--- lua/plugin-setup.lua
+++ lua/plugin-setup.lua
@@ -151,6 +151,8 @@ return packer.startup(function(use)
   -- git integration
   use("lewis6991/gitsigns.nvim") -- show line modifications on left hand side
 
+  use "karb94/neoscroll.nvim"
+
   if packer_bootstrap then
     require("packer").sync()
   end
