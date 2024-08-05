f45a4c4c3c1d2bfcc6270a3ad1e434173c6398f1
Kaylee Reed <kaylee@kaylee-macbookAir.local>
2024-08-05 12:22:24 +0200

    Adding netrw

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,5 +1,9 @@
 local opt = vim.opt
 
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 -- GLOBAL
 opt.iskeyword:append("-")
 opt.clipboard:append("unnamedplus")
