47f29cb07a0de32cf4ccef1dc6f1fa3daca0e1ac
Kiwi <kiwipetal@kittymail.com>
2024-10-18 16:39:37 +0200

    feat: Add dep plugins

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -1,3 +1,16 @@
 return { 
-
+  {
+    "nvim-lua/plenary.nvim",
+    lazy = true,
+    opts = {  }
+  },
+  {
+    "kyazdani42/nvim-web-devicons",
+    lazy = true,
+    opts = {  }
+  },
+  { 
+    'stevearc/dressing.nvim',
+    opts = {  }
+  }
 }
