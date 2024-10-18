77c48db780444c5653d994d9094ba4c84d1e5da5
Kiwi <kiwipetal@kittymail.com>
2024-10-18 16:39:13 +0200

    feat: Add nvimtree

---
+++ lua/plugins/nvimtree.lua
@@ -0,0 +1,15 @@
+return {
+  "kyazdani42/nvim-tree.lua",
+  requires = "kyazdani42/nvim-web-devicons" ,
+  wants = "nvim-web-devicons",
+  config = function()
+    require("nvim-web-devicons").setup()
+
+    require("nvim-tree").setup {
+      hijack_cursor = true,
+      view = {
+        width = 25
+      }
+    }
+  end
+}
