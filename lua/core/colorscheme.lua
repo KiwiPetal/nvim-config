fe410a453200f59f696860cb40e9abc49008e529
Kiwi <kiwipetal@kittymail.com>
2024-10-19 23:46:22 +0200

    fix: Update core

--- lua/core/colorscheme.lua
+++ lua/core/colorscheme.lua
@@ -1,4 +1,4 @@
-local status, _ = pcall(vim.cmd, "colorscheme tokyonight")
+local status, _ = pcall(vim.cmd, "colorscheme oxocarbon")
 if not status then
   print("Colorscheme not found!") -- print error if colorscheme not installed
   return
