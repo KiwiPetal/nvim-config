d9d2c41159afbc5fe582337a61d3774a931f11db
Kiwi <kiwipetal@kittymail.com>
2024-11-13 20:58:07 +0100

    fix: Push cmp capabilities into lsp

--- lua/core/lsp-config.lua
+++ lua/core/lsp-config.lua
@@ -4,17 +4,13 @@ vim.opt.signcolumn = 'yes'
 require("mason").setup()
 require("mason-lspconfig").setup()
 
-require("lspconfig").lua_ls.setup {}
-require("lspconfig").rust_analyzer.setup {}
-require("lspconfig").tailwindcss.setup {}
-require("lspconfig").ts_ls.setup {}
 local cmp = require("cmp")
 cmp.setup({
   snippet = {
     -- REQUIRED - you must specify a snippet engine
     expand = function(args)
       -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-      require('luasnip').lsp_expand(args.body)   -- For `luasnip` users.
+      require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
       -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
       -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
       vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
@@ -29,12 +25,12 @@ cmp.setup({
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.abort(),
-    ['<Tab>'] = cmp.mapping.confirm({ select = true }),   -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ['<Tab>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
   }),
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
     -- { name = 'vsnip' }, -- For vsnip users.
-    { name = 'luasnip', option = { use_show_condition = false, show_autosnippets = true } },   -- For luasnip users.
+    { name = 'luasnip', option = { use_show_condition = false, show_autosnippets = true } }, -- For luasnip users.
     -- { name = 'ultisnips' }, -- For ultisnips users.
     -- { name = 'snippy' }, -- For snippy users.
   }, {
@@ -54,12 +50,12 @@ cmp.setup({
  require("cmp_git").setup() ]] --
 
 -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline({ '/', '?' }, {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = {
-    { name = 'buffer' }
-  }
-})
+-- cmp.setup.cmdline({ '/', '?' }, {
+--   mapping = cmp.mapping.preset.cmdline(),
+--   sources = {
+--     { name = 'buffer' }
+--   }
+-- })
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
 -- cmp.setup.cmdline(':', {
@@ -81,3 +77,15 @@ cmp.event:on(
 -- Set up lspconfig.
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+require("lspconfig").lua_ls.setup {
+  capabilities = capabilities
+}
+require("lspconfig").rust_analyzer.setup {
+  capabilities = capabilities
+}
+require("lspconfig").tailwindcss.setup {
+  capabilities = capabilities
+}
+require("lspconfig").ts_ls.setup {
+  capabilities = capabilities
+}
