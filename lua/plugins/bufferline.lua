1c25478c165e558daab3d76c23baa60ed7c2d038
Kaylee Reed <kiwipetal@kittymail.com>
2025-01-10 20:51:35 +0100

    config: Remove bufferline offset for nvimtree

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -25,14 +25,6 @@ return {
       options = {
         show_buffer_close_icons = false,
         separator_style = "thick",
-        offsets = {
-          {
-            filetype = "NvimTree",
-            text = "Directory Tree",
-            highlight = "Directory",
-            text_align = "center",
-          },
-        },
         show_close_icon = false,
       }
     })
