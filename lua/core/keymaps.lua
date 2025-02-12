9ec8ad13621ad17ee81ece0093b59430ef7bcda1
Kaylee Reed <kiwipetal@kittymail.com>
2025-02-12 13:22:42 +0100

    feat(keymaps): Add git status telescope

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -54,7 +54,8 @@ keymap.set("n", "<leader>cc", "<cmd>CccPick<CR>")
 -- telescope
 keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>")       -- find files within current working directory, respects .gitignore
 keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>")        -- find string in current working directory as you type
-keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>")      -- find string under cursor in current working directory
+keymap.set("n", "<leader>fw", "<cmd>Telescope grep_string<cr>")      -- find string under cursor in current working directory
+keymap.set("n", "<leader>fc", "<cmd>Telescope git_status<cr>")       -- list git status
 keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>")          -- list open buffers in current neovim instance
 keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>")        -- list available help tags
 
