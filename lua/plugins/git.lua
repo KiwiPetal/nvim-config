590ddda0d14bb8a94ab0f66eeb3a1ac4c36a9ca0
Kaylee Reed <kiwipetal@kittymail.com>
2025-04-09 19:11:27 +0200

    style(git|telescope): Add transparency

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,13 +1,16 @@
 return {
-  { 
-    "kdheepak/lazygit.nvim", 
-    dependencies = { "nvim-lua/plenary.nvim" } 
+  {
+    "kdheepak/lazygit.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      vim.g.lazygit_floating_window_winblend = 50
+    end
   },
-  { 
-    "NeogitOrg/neogit", 
-    dependencies = { 
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
       "nvim-lua/plenary.nvim",
       "sindrets/diffview.nvim",
-    } 
+    }
   },
 }
